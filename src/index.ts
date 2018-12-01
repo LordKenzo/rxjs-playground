98877e8868b8fe79aa75e8cbdfa469a64c544bf1
LordKenzo <lordkenzo@outlook.it>
2018-12-01 19:21:36 +0100

    Add jquery (and @types/jquery) and moment to the project

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import { Observable, of } from 'rxjs';
+import moment from 'moment';
+import $ from 'jquery';
 
 let ob: Observable<number> = of(1, 2, 3);
 ob.subscribe(data => console.log(data));
+$('body').text(moment().format());
